0c97972b6e39faf377563d857c09e439e7d9292f
Algac <66782963+Sergear00s@users.noreply.github.com>
2024-11-21 07:17:13 +0000

    __all__

--- ftlib/Users.py
+++ ftlib/Users.py
@@ -4,7 +4,6 @@ class User:
     def __init__(self, data : dict, api) -> None:
         self.data = data
         self.__api = api
-
     def add_correction(self, reason: str,amount : int = 1):
         """
             Adds correction point to self login.
--- ftlib/__init__.py
+++ ftlib/__init__.py
@@ -3,3 +3,6 @@ from .Users import Users
 from .journal import Journal
 from .Cursus import Cursus
 from .Constants import *
+
+
+__all__ = ["Ftlib", "Users", "Journal", "Cursus", "Constants"]
