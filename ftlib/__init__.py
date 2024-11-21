1fa2ab886d65007d8525f561a586412276557ee5
Algac <66782963+Sergear00s@users.noreply.github.com>
2024-11-21 10:17:32 +0000

    imports fixed

--- ftlib/__init__.py
+++ ftlib/__init__.py
@@ -1,6 +1,8 @@
 from ._Ftlib import Ftlib
-from .users._Users import Users
-from .journal._Journal import Journal
-from .cursus._Cursus import Cursus
-from ._Constants import *
+import exceptions
+import curses
+import journal
+import users
+import projects
+
 
