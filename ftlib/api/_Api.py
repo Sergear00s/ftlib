89518cb448833acc8dd7f951da32c40b2b1d0d16
Algac <66782963+Sergear00s@users.noreply.github.com>
2024-11-22 09:46:03 +0000

    params

--- ftlib/api/_Api.py
+++ ftlib/api/_Api.py
@@ -10,13 +10,17 @@ class Api:
         self.__api.tokener()
         header = self.__api.header
         url = self.__api.endpoint + endpoint
-        return requests.request("get", url, params=params, headers=header, **kwargs)
+        kwargs["params"]=params
+        kwargs["header"]= header
+        return requests.request("get", url, **kwargs)
     
-    def post(self, endpoint, data=None, json=None, **kwargs):
+    def post(self, endpoint, data=None, **kwargs):
         self.__api.tokener()
         header = self.__api.header
         url = self.__api.endpoint + endpoint
-        return requests.request("post", url, headers=header, data=data, json=json, **kwargs)
+        kwargs["data"]=data
+        kwargs["header"]= header
+        return requests.request("post", url, **kwargs)
     
     def put(self, endpoint, data=None, json=None, **kwargs):
         self.__api.tokener()
