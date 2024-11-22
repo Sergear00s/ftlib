64a1218adc9dfc4ac570cddb38c5463100d3f2c0
Algac <66782963+Sergear00s@users.noreply.github.com>
2024-11-22 10:16:41 +0000

    param fixed

--- ftlib/wallet/_Transaction.py
+++ ftlib/wallet/_Transaction.py
@@ -21,7 +21,7 @@ class Transaction:
                 "reason": str
             }
         }
-        resp = self.__api.api.post("/v2/transactions", headers=self.__api.header, data=data)
+        resp = self.__api.api.post("/v2/transactions", data=data)
         self.__api.eval_resp(resp)
 
 
