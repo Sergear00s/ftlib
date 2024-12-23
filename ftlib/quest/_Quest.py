a99e5fea51c7273b3d9b6c2266691f7b3330da80
Algac <66782963+Sergear00s@users.noreply.github.com>
2024-12-23 11:38:32 +0000

    fpusgh

--- ftlib/quest/_Quest.py
+++ ftlib/quest/_Quest.py
@@ -4,7 +4,6 @@ class Quest:
     def __init__(self, api) -> None:
         self.__api = api
     
-
     def get_user_quests(self, user_id : str):
         """GET /v2/users/:user_id/quests"""
 
@@ -12,9 +11,9 @@ class Quest:
         data = self.__api.format_page_resp(data)
         return data
     
-    def get_campus_quests(self, campus_id : int):
+    def get_campus_users_quests(self, campus_id : int):
          """/v2/campus/:campus_id/quests"""
 
-         data = self.__api.Api.page("/v2/campus/{}/quests".format(campus_id))
+         data = self.__api.Api.page("/v2/quests", params={"filter[campus_id]":campus_id})
          data = self.__api.format_page_resp(data)
          return data
