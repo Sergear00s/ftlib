9314934b2c4816e3eed324f46d9fb69fc2f50753
Algac <66782963+Sergear00s@users.noreply.github.com>
2025-01-15 07:34:32 +0000

    achivement

--- ftlib/achivement/_Achivement.py
+++ ftlib/achivement/_Achivement.py
@@ -3,3 +3,10 @@
 class Achivement:
     def __init__(self, root) -> None:
         self.__api = root
+
+
+
+    def achive(self, user_id : str, achivement_id : int):
+        """POST /v2/achievements_users"""
+        self.__api.Api.post("/v2/achievements_users", json={"achivement_id": achivement_id, "user_id":user_id})
+        return
